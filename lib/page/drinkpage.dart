27e818e8897a70d09dfab64dd506bf92d1dde5f4
Your Name <you@example.com>
2023-01-12 18:53:14 +0700

    Third commit

--- lib/page/drinkpage.dart
+++ lib/page/drinkpage.dart
@@ -2,13 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import 'descriptionpage.dart';
+
 class DrinkPage extends StatelessWidget {
   const DrinkPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("ini DrinkPage")),
+        body: Container(
+      margin: EdgeInsets.only(left: 15, right: 15),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            mainAxisExtent: 200),
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => DescriptionPage()));
+            },
+            child: Container(
+              // color: Colors.amber,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.black,
+                    width: 110,
+                    height: 110,
+                  ),
+                  Divider(
+                    thickness: 2,
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "title",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(left: 10),
+                      alignment: Alignment.centerLeft,
+                      child: Text("Subtitle",
+                          style: TextStyle(
+                            fontSize: 10,
+                          ))),
+                  Container(
+                      margin: EdgeInsets.only(right: 10),
+                      alignment: Alignment.centerRight,
+                      child: Text("Price",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold)))
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    )
     );
   }
 }
